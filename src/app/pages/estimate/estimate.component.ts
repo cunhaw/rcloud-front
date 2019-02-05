a6a6147cf0daa9614babda274aedfa86d118c0fb
Wagner <cunhaw@gmail.com>
2019-02-05 00:01:12 -0200

    Upload file

--- src/app/pages/estimate/estimate.component.ts
+++ src/app/pages/estimate/estimate.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
 
+import { AngularFileUploaderModule } from "angular-file-uploader";
+
 @Component({
   selector: 'ngx-estimate',
   templateUrl: './estimate.component.html',
 })
 export class EstimateComponent {
+  afuConfig = {
+    multiple: false,
+    formatsAllowed: ".ls8,.ls7",
+    maxSize: "250",
+    uploadAPI:  {
+      url:"https://example-file-upload-api",
+      headers: {
+     "Content-Type" : "text/plain;charset=UTF-8",
+     //"Authorization" : `Bearer ${token}`
+     "Authorization" : `Bearer xx`
+      }
+    },
+    //theme: "",
+    hideProgressBar: false,
+    hideResetBtn: false,
+    hideSelectBtn: false
+  };
 }
--- src/app/pages/estimate/estimate.module.ts
+++ src/app/pages/estimate/estimate.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 import { ThemeModule } from '../../@theme/theme.module';
 import { EstimateComponent } from './estimate.component';
 
+import { AngularFileUploaderModule } from "angular-file-uploader";
+
 @NgModule({
   imports: [
-    ThemeModule
+    ThemeModule,
+    AngularFileUploaderModule
   ],
   declarations: [
     EstimateComponent,
@@ -17,5 +20,3 @@ import { EstimateComponent } from './estimate.component';
 export class EstimateModule { 
  
 };
-}
- 
